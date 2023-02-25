201ea9e6f6113cafed9010bb715cc507888ec7f5
31steets31 <stepaha78@gmail.com>
2023-02-25 16:24:15 +0300

    Updating rotate figure and some fixes

--- lab_02/main.py
+++ lab_02/main.py
@@ -1,8 +1,9 @@
 import math
-from decimal import Decimal
-
-from PyQt5 import QtWidgets, QtCore, QtGui, uic
 import sys
+
+from decimal import Decimal
+from PyQt5 import QtWidgets, QtCore, QtGui, uic
+
 
 # Default coordinates
 default_coordinates = {
@@ -10,15 +11,15 @@ default_coordinates = {
         "first_point": [450, 410],
         "second_point": [410, 330],
         "third_point": [490, 330],
+        "radius": 50,
     },
     "line": {
         "first_point": [350, 410],
         "second_point": [550, 410]
     },
     "ellipse": {
-        "center": [350, 360],
-        "radius_x": 200,
-        "radius_y": 100
+        "radius_x": 100,
+        "radius_y": 50
     },
     "left_quad": {
         "first_side": {
@@ -34,10 +35,14 @@ default_coordinates = {
     }
 }
 
-# Colors code: green, red, blue, purple, yellow
-colors = ["#00FF00", "#FF0000", "#0000FF", "#800080", "#FFFF00"]
+# Transfers coordinates to the coordinate system of the canvas
+DX, DY = 450, 360
+
+# Colors code: red, purple, yellow
+colors = ["#FF0000", "#800080", "#FFFF00"]
 
 
+# Print error message
 def ErrorDialog(title, name, info):
 
     # Create a message box
@@ -54,14 +59,18 @@ def ErrorDialog(title, name, info):
     msg.exec_()
 
 
+# Rotate the point around the center
 def RotatePoint(center, point, angle):
 
-    return [int((Decimal(center[0] + (point[0] - center[0]) * Decimal(math.cos(math.radians(angle)))) + Decimal((point[1] - center[1])) *
-                Decimal(math.sin(math.radians(angle))))),
-            int((Decimal(center[1] - (point[0] - center[0]) * Decimal(math.sin(math.radians(angle)))) + Decimal((point[1] - center[1])) *
-                Decimal(math.cos(math.radians(angle)))))]
+    return [(Decimal(center[0] + (point[0] - center[0]) * Decimal(math.cos(math.radians(angle)))) +
+             Decimal((point[1] - center[1])) *
+             Decimal(math.sin(math.radians(angle)))),
+            (Decimal(center[1] - (point[0] - center[0]) * Decimal(math.sin(math.radians(angle)))) +
+             Decimal((point[1] - center[1])) *
+             Decimal(math.cos(math.radians(angle))))]
 
 
+# Get the coordinates of the point
 def GetXYParams(func_x, func_y):
 
     tmp_x = func_x()
@@ -87,9 +96,10 @@ def GetXYParams(func_x, func_y):
 
         return [], ValueError
 
-    return [int(x), int(y)], None
+    return [int(x) + DX, int(y) + DY], None
 
 
+# Get the center of the circle
 def GetCircleCenter(first_point, second_point, third_point):
 
     x1, y1 = first_point
@@ -108,16 +118,42 @@ def GetCircleCenter(first_point, second_point, third_point):
     xc = (Decimal((m1 * m2 * (y1 - y3) + m2 * (x1 + x2) - m1 * (x2 + x3)))) / (Decimal(2 * (m2 - m1)))
     yc = (Decimal(-1 / m1)) * (Decimal(xc - Decimal(Decimal((x1 + x2)) / 2))) + Decimal(Decimal((y1 + y2)) / 2)
 
-    return [int(xc - 50), int(yc - 50)]
+    return [int(xc), int(yc)]
 
 
-def GetCircleRadius(first_point, second_point, third_point, center):
+# Get the length of the vector in the X direction
+def GetDXLen(input_len, angle):
 
-    r1 = Decimal(math.sqrt(Decimal((first_point[0] - center[0]) ** 2) + Decimal((first_point[1] - center[1]) ** 2)))
-    r2 = Decimal(math.sqrt(Decimal((second_point[0] - center[0]) ** 2) + Decimal((second_point[1] - center[1]) ** 2)))
-    r3 = Decimal(math.sqrt(Decimal((third_point[0] - center[0]) ** 2) + Decimal((third_point[1] - center[1]) ** 2)))
+    return input_len * math.cos(angle * math.pi / 180.0)
 
-    return int(max(r1, r2, r3))
+
+# Get the length of the vector in the Y direction
+def GetDYLen(input_len, angle):
+
+    return input_len * math.sin(angle * math.pi / 180.0)
+
+
+# Get angle between two lines
+def GetAngle(first_point, second_point, third_point):
+
+    x1, y1 = first_point
+    x2, y2 = second_point
+    x3, y3 = third_point
+
+    if x1 == x2 or x2 == x3:
+        x2 += 1e-11
+
+    m1 = (Decimal((y2 - y1))) / (Decimal((x2 - x1)))
+    m2 = (Decimal((y3 - y2))) / (Decimal((x3 - x2)))
+
+    if m1 == 0 or m1 == m2:
+        m1 += Decimal(1e-11)
+
+    angle = math.degrees(math.atan((m2 - m1) / (1 + m1 * m2)))
+
+    print("Angle: ", angle)
+
+    return angle
 
 
 class Window(QtWidgets.QMainWindow):
@@ -176,27 +212,26 @@ class Window(QtWidgets.QMainWindow):
                       self.coordinates["line"]["second_point"])
 
         self.DrawHalfEllipse(colors[2],
-                             self.coordinates["ellipse"]["center"],
                              self.coordinates["ellipse"]["radius_x"],
                              self.coordinates["ellipse"]["radius_y"])
 
-        self.DrawLine(colors[3],
+        self.DrawLine(colors[1],
                       self.coordinates["left_quad"]["first_side"]["first_point"],
                       self.coordinates["left_quad"]["first_side"]["second_point"])
 
-        self.DrawLine(colors[3],
+        self.DrawLine(colors[1],
                       self.coordinates["left_quad"]["first_side"]["second_point"],
                       RotatePoint(self.coordinates["left_quad"]["first_side"]["second_point"],
                                   self.coordinates["left_quad"]["first_side"]["first_point"],
                                   -90))
 
-        self.DrawLine(colors[3],
+        self.DrawLine(colors[1],
                       self.coordinates["left_quad"]["first_side"]["first_point"],
                       RotatePoint(self.coordinates["left_quad"]["first_side"]["first_point"],
                                   self.coordinates["left_quad"]["first_side"]["second_point"],
                                   90))
 
-        self.DrawLine(colors[3],
+        self.DrawLine(colors[1],
                       RotatePoint(self.coordinates["left_quad"]["first_side"]["first_point"],
                                   self.coordinates["left_quad"]["first_side"]["second_point"],
                                   90),
@@ -204,23 +239,23 @@ class Window(QtWidgets.QMainWindow):
                                   self.coordinates["left_quad"]["first_side"]["first_point"],
                                   -90))
 
-        self.DrawLine(colors[3],
+        self.DrawLine(colors[1],
                       self.coordinates["right_quad"]["first_side"]["first_point"],
                       self.coordinates["right_quad"]["first_side"]["second_point"])
 
-        self.DrawLine(colors[3],
+        self.DrawLine(colors[1],
                       self.coordinates["right_quad"]["first_side"]["second_point"],
                       RotatePoint(self.coordinates["right_quad"]["first_side"]["second_point"],
                                   self.coordinates["right_quad"]["first_side"]["first_point"],
                                   90))
 
-        self.DrawLine(colors[3],
+        self.DrawLine(colors[1],
                       self.coordinates["right_quad"]["first_side"]["first_point"],
                       RotatePoint(self.coordinates["right_quad"]["first_side"]["first_point"],
                                   self.coordinates["right_quad"]["first_side"]["second_point"],
                                   -90))
 
-        self.DrawLine(colors[3],
+        self.DrawLine(colors[1],
                       RotatePoint(self.coordinates["right_quad"]["first_side"]["first_point"],
                                   self.coordinates["right_quad"]["first_side"]["second_point"],
                                   -90),
@@ -229,6 +264,13 @@ class Window(QtWidgets.QMainWindow):
                                   90))
 
     def TransferFigure(self, shift):
+
+        # Check if the figure was drawn
+        if len(self.coordinates) == 0:
+            ErrorDialog("Ошибка переноса",
+                        "Невозможно переместить фигуру, так как она не была нарисована",
+                        "")
+            return
 
         self.coordinates["circle"]["first_point"][0] += shift[0]
         self.coordinates["circle"]["first_point"][1] -= shift[1]
@@ -257,6 +299,13 @@ class Window(QtWidgets.QMainWindow):
 
     def RotateFigure(self, center, angle):
 
+        # Check if the figure was drawn
+        if len(self.coordinates) == 0:
+            ErrorDialog("Ошибка поворота",
+                        "Невозможно повернуть фигуру, так как она не была нарисована",
+                        "")
+            return
+
         self.coordinates["circle"]["first_point"] = \
             RotatePoint(center, self.coordinates["circle"]["first_point"], angle)
         self.coordinates["circle"]["second_point"] = \
@@ -268,9 +317,6 @@ class Window(QtWidgets.QMainWindow):
             RotatePoint(center, self.coordinates["line"]["first_point"], angle)
         self.coordinates["line"]["second_point"] = \
             RotatePoint(center, self.coordinates["line"]["second_point"], angle)
-
-        self.coordinates["ellipse"]["center"] = \
-            RotatePoint(center, self.coordinates["ellipse"]["center"], angle)
 
         self.coordinates["left_quad"]["first_side"]["first_point"] = \
             RotatePoint(center, self.coordinates["left_quad"]["first_side"]["first_point"], angle)
@@ -332,8 +378,6 @@ class Window(QtWidgets.QMainWindow):
                             "Ожидались целые величины\n")
                 return
 
-        # Очистить поле и нарисовать фигуру после всех преобразований
-
         tmp_coordinates = self.coordinates
         self.DeleteGraph()
         self.coordinates = tmp_coordinates
@@ -348,12 +392,11 @@ class Window(QtWidgets.QMainWindow):
         # Set the painter
         painter = QtGui.QPainter(self.GraphField.pixmap())
         pen = QtGui.QPen()
-        pen.setWidth(2)
         pen.setColor(QtGui.QColor(color))
         painter.setPen(pen)
 
         # Draw the line
-        painter.drawLine(first_point[0], first_point[1], second_point[0], second_point[1])
+        painter.drawLine(int(first_point[0]), int(first_point[1]), int(second_point[0]), int(second_point[1]))
 
         # Update the field
         self.GraphField.update()
@@ -364,7 +407,6 @@ class Window(QtWidgets.QMainWindow):
         # Set the painter
         painter = QtGui.QPainter(self.GraphField.pixmap())
         pen = QtGui.QPen()
-        pen.setWidth(2)
         pen.setColor(QtGui.QColor(color))
         painter.setPen(pen)
 
@@ -373,30 +415,58 @@ class Window(QtWidgets.QMainWindow):
                                  self.coordinates["circle"]["second_point"],
                                  self.coordinates["circle"]["third_point"])
 
-        # Get radius
-        radius = GetCircleRadius(self.coordinates["circle"]["first_point"],
-                                 self.coordinates["circle"]["second_point"],
-                                 self.coordinates["circle"]["third_point"],
-                                 center) + 25
-
         # Draw the circle
-        painter.drawEllipse(center[0], center[1], radius, radius)
+        last_coord = [center[0] + self.coordinates["circle"]["radius"], center[1]]
+
+        for i in range(3600):
+            new_coord = [center[0] + GetDXLen(self.coordinates["circle"]["radius"], i * 0.1),
+                         center[1] - GetDYLen(self.coordinates["circle"]["radius"], i * 0.1)]
+
+            # Draw the line
+            painter.drawLine(int(last_coord[0]), int(last_coord[1]), int(new_coord[0]), int(new_coord[1]))
+
+            last_coord = new_coord
 
         # Update the field
         self.GraphField.update()
         painter.end()
 
-    def DrawHalfEllipse(self, color, center, radius_x, radius_y):
+    def DrawHalfEllipse(self, color, radius_x, radius_y):
 
         # Set the painter
         painter = QtGui.QPainter(self.GraphField.pixmap())
         pen = QtGui.QPen()
-        pen.setWidth(2)
         pen.setColor(QtGui.QColor(color))
         painter.setPen(pen)
 
+        # Get initial point
+        last_coord = [self.coordinates["line"]["first_point"][0], self.coordinates["line"]["first_point"][1]]
+
+        # Get center
+        center = [Decimal(last_coord[0] + radius_x), Decimal(last_coord[1])]
+
+        # Get angle between the center line and the x-axis
+        angle = GetAngle(self.coordinates["line"]["second_point"],
+                         self.coordinates["line"]["first_point"],
+                         center)
+
+        if angle < 0 and self.coordinates["line"]["second_point"][1] < self.coordinates["line"]["first_point"][1] or \
+                angle > 0 and self.coordinates["line"]["second_point"][1] > self.coordinates["line"]["first_point"][1]:
+            angle += 180
+
         # Draw the half ellipse
-        painter.drawArc(center[0], center[1], radius_x, radius_y, 0, -180 * 16)
+        for i in range(1800):
+
+            new_coord = RotatePoint(
+                self.coordinates["line"]["first_point"],
+                [center[0] + Decimal(GetDXLen(radius_x, i * 0.1)),
+                 center[1] + Decimal(GetDYLen(radius_y, i * 0.1))],
+                angle)
+
+            # Draw the line
+            painter.drawLine(int(last_coord[0]), int(last_coord[1]), int(new_coord[0]), int(new_coord[1]))
+
+            last_coord = new_coord
 
         # Update the field
         self.GraphField.update()
